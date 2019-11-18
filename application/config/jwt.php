a26a94d611c871cd21c1b06f70e7bc74d800da7f
Greg <chrisnamahendra@gmail.com>
2019-11-18 08:01:38 +0700

    tugas paw

---
+++ application/config/jwt.php
@@ -0,0 +1,11 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['jwt_key'] = ' 6b0505a44d4164f2b5790943b1f43e64';
+
+/*Generated token will expire in 1 minute for sample code
+* Increase this value as per requirement for production
+*/
+$config['token_timeout'] = 1;
+
+/* End of file jwt.php */
+/* Location: ./application/config/jwt.php */
